777179890d50d8fb2f6927cb97d9a5026b1ef017
Mohd Ismaeel <pmohd367@gmail.com>
2025-03-15 19:38:20 +0530

    Version 1.0.0 Ready To Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8800";
+export const BASE_URL = "https://proconnect-3s6l.onrender.com";
 
 const clientServer = axios.create({
     baseURL: BASE_URL,
